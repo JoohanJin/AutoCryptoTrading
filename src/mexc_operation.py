40beec04bee61972fac58557dd456448f1322c11
JoohanJin <wngks3146@naver.com>
2024-01-21 00:03:00 +0800

    new start

---
+++ src/mexc_operation.py
@@ -0,0 +1,9 @@
+from mexc_client import *
+
+# Global Variables
+# end point list
+BASE_ENDPOINT: str = "https://api.mexc.com"
+TEST_CONNECT_PATH: str = "/api/v3/ping"
+TEST_SERVER_TIME_PATH: str = "/api/v3/time"
+RECENT_TRADE_LIST_PATH: str = "/api/v3/trades"
+EXCHANGE_INFO_PATH: str = "/api/v3/exchangeInfo"
