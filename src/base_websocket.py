1b11a111eda5ae9eb975e19537479f50d3fe893f
JoohanJin <wngks3146@naver.com>
2024-05-24 01:11:13 +0800

    feat: implemented __init__ of websocket base - not yet done

--- src/base_websocket.py
+++ src/base_websocket.py
@@ -17,7 +17,7 @@ SPOT: str = None # for now
 class _WebSocketManager:
     def __init__(
             self,
-            # callback_function,
+            callback_function,
             websocket_name,
             api_key: Optional[str] = None,
             secret_key: Optional[str] = None,
@@ -25,14 +25,46 @@ class _WebSocketManager:
             ping_timeout: Optional[int] = 10,
             retries: Optional[int] = 10,
             restart_on_error: Optional[bool] = True,
-            trace_logging: Optional[bool] = False
+            # trace_logging: Optional[bool] = False
         ) -> None:
 
+        # Set API key
         self.api_key = api_key
         self.secret_key = secret_key
 
+        # get the callback function
+        self.callback = callback_function
+
+        # get the websocket and store in the class
         self.ws = websocket_name
+
+        if (api_key):
+            self.ws += " (Auth)"
+
+        # ping settings
+        self.ping_interval = ping_interval
+        self.ping_timeout = ping_timeout
+        self.retry_count = retries
+
+        # to save the list of subcriptions and the function for each subcription
+        """
+        {
+            <subscription-type>: callback function
+        }
+        """
+        self.callback_directory = {}
+
+        self.restart_on_error = restart_on_error
+
         return
+    
+    def exit(self):
+        """
+        close the wevsocket
+        """
+        self.ws.close()
+    
+
     
 
 class _FutureWebsocketManager(_WebSocketManager):
