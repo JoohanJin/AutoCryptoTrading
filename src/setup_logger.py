32cb228d72798a1fa73145d20d3412d9d39a328b
JoohanJin <wngks3146@naver.com>
2024-05-26 08:40:26 +0800

    func: universal logger and simple test

--- src/setup_logger.py
+++ src/setup_logger.py
@@ -1,14 +1,25 @@
 import logging
+import time
 
 # config the basic log format
 logging.basicConfig(
-    filename="test.log",
+    filename=f"{time.strftime('%Y-%m-%d', time.localtime(time.time()))}.log",
     encoding='utf-8',
     format="%(asctime)s - %(levelname)s - %(message)s",
     level=logging.INFO
     )
-logger = logging.getLogger("base_logger")
+logger = logging.getLogger("bot_logger")
 
+if __name__=="__main__":
+    logger.info("test")
+
+"""
+logging.basicConfig(
+    filename = f"{time.strftime("%Y-%m-%D %H:%M:%S %Z", time.localtime(time.time()))}",
+    filemode='a', # append, if the file exists, create new if there is no such file
+    level="INFO"
+)
+"""
 
 """
 # testing logging library
