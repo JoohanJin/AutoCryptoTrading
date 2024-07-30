647b69277e939e66fa963aff013c32cc2f770a29
JoohanJIN <wngks3146@naver.com>
2024-07-30 23:50:13 +0800

    feat:

--- src/telegram/telegram_bot_class.py
+++ src/telegram/telegram_bot_class.py
@@ -2,40 +2,39 @@ from telegram import Bot
 import json
 import asyncio
 
-
 class Test:
     def __init__(self) -> None:
-        self._telegram_bot = TelegramBot()
+        self._telegram_bot = CustomTelegramBot();
 
     async def print_hello_world(self) -> str:
-        print("Hello World") # Doing Some Synchronous Task
+        print("Hello World"); # Doing Some Synchronous Task
 
-        await self._telegram_bot.send_text("Hello World")
+        await self._telegram_bot.send_text("Hello World");
 
-        return "Hello World"
+        return "Hello World";
 
-class TelegramBot:
+class CustomTelegramBot:
     def __init__(self) -> None:
         # Get the credential from the json
         # will be considered as private attributes.
-        self.__api_key, self.__channel_id = self.get_credential()
+        self.__api_key, self.__channel_id = self.get_credential();
         
         # make the bot instance
-        self._bot = Bot(self.__api_key)
-        return
+        self._bot = Bot(self.__api_key);
+        return;
 
     def get_credential(self) -> str:
         # funcion called when TelegramBot is initialized.
-        f = open('key.json')
-        data = json.load(f)
-        return data['api_key'], data['channel_id']
+        f = open('key.json');
+        data = json.load(f);
+        return data['api_key'], data['channel_id'];
 
     async def send_text(self, message: str) -> None:
         await self._bot.send_message(
             chat_id = self.__channel_id,
             text = message,
-        )
-        return
+        );
+        return;
 
 
 ############################################################################################
@@ -43,9 +42,8 @@ class TelegramBot:
 ############################################################################################
 
 async def main():
-    test = Test()
-    await test.print_hello_world()
-
+    test = Test();
+    await test.print_hello_world();
 
 if (__name__ == "__main__"):
-    asyncio.run(main())
+    asyncio.run(main());
