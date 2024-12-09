63ebc0a68da1bc5d6097e3c1f5eec4cf8e83fd64
Joohan Jin <wngks3146@naver.com>
2024-12-09 21:42:12 +0800

    chord: comments have been added and more specific exception handling has been added

--- src/data_collector_and_processor.py
+++ src/data_collector_and_processor.py
@@ -23,20 +23,21 @@ class DataCollectorAndProcessor:
         # so that it can subscribe different values at the initiation.
     ) -> None:
         """
-        :Function Name: __init__() for StrategyManager
-            :set the WebSocket() for data fetching
-                :set the subscription for the ticker data.
-            :set the DataSaver() for control of the DataFrame Size.
-            :set the Threads pool for the necessary operations for the Strategy Manager.
-            :set the Telegram Bot for the automated log system.
-            :set the DataBuffer for the price buffer.
-            :set the Price Data Table
+        # func __init__() for StrategyManager
+            # set the WebSocket() for data fetching
+                # set the subscription for the ticker data.
+            # set the DataSaver() for control of the DataFrame Size.
+            # set the Threads pool for the necessary operations for the Strategy Manager.
+            # set the Telegram Bot for the automated log system.
+            # set the DataBuffer for the price buffer.
+            # set the Price Data Table
 
-        :params self:
-        :params pipeline:
-            :pipeline to transmit the data to the data processor.
+        # params self
+            # class object
+        # params pipeline
+            # pipeline to transmit the data to the StrategyManager.
 
-        :return None:        
+        # return None
         """
         # it will automatically connect the websocket to the host
         # and will continue to keep the connection between the client and host
@@ -86,12 +87,15 @@ class DataCollectorAndProcessor:
     """
     def _init_threads(self) -> None:
         """
-        :function name: _init_threads():
-            :set the threads for the necessary operations and append them into the list of thread pool
+        # func _init_threads():
+            # set the threads for the necessary operations and append them into the list of thread pool
 
-        :params self:
+        # params self
+            # class object
 
-        :The list of threads are as follows:
+        # return None
+
+        # The list of threads are as follows:
             :price fetching
                 :get the price from the broker
             :calculate the simple moving average
@@ -188,35 +192,50 @@ class DataCollectorAndProcessor:
     """
     def _price_data_fetch(self) -> None:
         '''
-            Continuously fetches data from the queue, processes it and appends it to the DataFrame.
+        # func _price_data_fetch():
+            # It continuously fetches data from the queue, processes it and appends it to the DataFrame.
+            # the processing includes the followings:
+                # change the data to a dataframe
+                # get the timestamp
+                # set the timestamp as an index of the dataframe
+                # concatenate the dataframe to the entire data buffer.
         '''
-        # consider append each data into the list and convert it to df periodically.
-        # data_buffer: list = list()
-        # timestamp_buffer: list = list()
-        # buffer_size: int = 3
         while True:
             try:
-                response: dict = self._get_data_buffer()
+                response: dict = self._get_data_buffer() # data from the data buffer
                 if response:
                     # TODO: store 'riseFallRates' and 'riseFallRatesTimezone'
                     timestamp = response['timestamp']
-                    # timestamp_buffer.append(timestamp)
-                    # data_buffer.append(response)
 
+                    # make the data, dictionary, into the pandas dataframe.
                     tmp = pd.DataFrame(
                         data = [response],
                     )
 
-                    tmp.set_index("timestamp", inplace = True)
+                    # set the timestamp as the index of the dataframe.
+                    tmp.set_index(
+                        "timestamp",
+                        inplace = True
+                    )
 
+                    # merge the new dataframe to the existing dataframe.
                     with self.df_lock:
-                        self.priceData=pd.concat([self.priceData, tmp], axis=0)
+                        self.priceData = pd.concat(
+                            [self.priceData, tmp],
+                            axis=0
+                        )
 
             except Exception as e:
                 logger.critical(f"Unexpected Error Occurred in function \"_price_data_fetch\": {e}")
         return
     
     def _get_data_buffer(self) -> Optional[dict]:
+        """
+        # func _get_data_buffer():
+            # Get the data from the buffer and return it.
+            # if there is no data in the buffer, then wait until the data is available.
+            # if there is an error then, return None
+        """
         try:
             result = self.price_fetch_buffer.get(block = True)
 
@@ -224,7 +243,7 @@ class DataCollectorAndProcessor:
 
             return result
         except Exception as e:
-            logger.critical(f"Error retreving data from queue: {e}")
+            logger.critical(f"{__name__} - Error retreving data from queue: {e}")
             return None
 
     # for batch processing of the data.
@@ -233,6 +252,11 @@ class DataCollectorAndProcessor:
         data_buffer: list,
         timestamp_buffer: list,
     ) -> bool:
+        """
+        # func __append_df():
+            # Make the data as an Pandas dataframe.
+            # store the dataframe into the list, for the batch processing
+        """
         try:
             tmp = pd.DataFrame(
                 data_buffer,
@@ -253,6 +277,14 @@ class DataCollectorAndProcessor:
     ######################################################################################################################
     """
     def _calculate_moving_averages(self) -> None:
+        """
+        # func _calculate_moving_averages():
+            # call the function __calculate_ema_sma() to calculate the EMA and SMA
+            # get tuple of data where:
+                # data[0] = SMA values
+                # data[1] = EMA values
+            # when the data is available, push the data to the data pipeline.
+        """
         while True:
             data: Tuple[Tuple[float]] | None = self.__calculate_ema_sma()
 
@@ -264,9 +296,6 @@ class DataCollectorAndProcessor:
                 if (sma_values): self.__push_sma_data(sma_values)
                 if (ema_values): self.__push_ema_data(ema_values)
 
-            # if (sma_values and ema_values):
-            #     sma_5, sma_10, sma_15, sma_20 = sma_values
-            #     ema_5, ema_10, ema_15, ema_20 = ema_values
             time.sleep(2)
         return
     
@@ -283,34 +312,45 @@ class DataCollectorAndProcessor:
         ),
     ) -> Optional[Tuple[Tuple[float], Tuple[float]]]:
         """
-        Calculate the simple moving average (SMA) of the lastPrice
+        # func __calculate_ema_sma():
+            # It calculate the simple moving average (SMA) of the lastPrice
 
-        :params periods
-        :returns: The calculated SMA or None if there is not enough data
-        # TODO: May be able to add context manager in python
-            # For safer lock handling for  timeout.
-            # Makes SMA periods configurable.
+        # params self: DataCollectorAndProcessor
+            # class object
+        # params periods: Tuple[int]
+            # periods for the calculation of the SMA and EMA
+
+        # return (smas, emas): Tuple[Tuple[float], Tuple[float]] | None
+            # Tuple of SMA and EMA values
         """
-        self.df_lock.acquire()
 
         try:
-            if (self.priceData.shape[0] < periods[-1]):
-                # logger.warning(f"MA period ({self.priceData.shape[0]}) is less than maximum SMA period ({periods[-1]})")
-                return
+            with self.df_lock:
+                if (self.priceData.shape[0] < periods[-1]):
+                    return None
 
-            tmp = self.priceData[-periods[-1]:]["lastPrice"].copy()
-        except Exception as e:
-            logger.warnning(f"from {__name__}, function: {self}.__calculate_ema_sma has has raised the Exception")
-            return
-        finally:
-            self.df_lock.release()
+                tmpDataframe = self.priceData[-periods[-1]:]["lastPrice"].copy()
+
+            smas = tuple(np.mean(tmpDataframe[-period:]) for period in periods)
+            emas = tuple(pd.Series(tmpDataframe).ewm(span=period, adjust = False).mean().iloc[-1] for period in periods)
         
-        smas = tuple(np.mean(tmp[-period:]) for period in periods)
-        emas = tuple(
-            pd.Series(tmp).ewm(span=period, adjust = False).mean().iloc[-1]
-            for period in periods
-        )
-        return smas, emas
+            return smas, emas
+
+        except KeyError as e:
+            # Specific error handling for KeyError, i.e., missing collumn
+            logger.error(f"{__name__}: function {self.__class__.__name__}.__calculate_ema_sma has raised the KeyError: {e}")
+            return None
+        
+        except IndexError as e:
+            # Specific error handling for IndexError, i.e., out of range and slicing of the DataFrame.
+            logger.error(f"{__name__}: function {self.__class__.__name__}.__calculate_ema_sma has raised the IndexError: {e}")
+            return None
+
+        except Exception as e:
+            logger.warning(f"{__name__}: function {self.__class__.__name__}.__calculate_ema_sma has has raised the Unknown Exception.")
+            return None
+        
+
     
     """
     ######################################################################################################################
@@ -319,10 +359,15 @@ class DataCollectorAndProcessor:
     """
     def _resize_df(self) -> None:
         """
-        :func: __save_data()
-            : using _data_saver to move the dataframe stroing the price movement to the csv file in data
+        # func: __resize_df()
+            # using _data_saver to move the dataframe storing the price movement to the csv file in data
         
-        :make a use of data saver, i.e., custom class using the df.to_csv()
+        # make a use of data saver, i.e., custom class using the df.to_csv()
+
+        # params self: DataCollectorAndProcessor
+            # class object
+
+        # return None
         """
         while True:
             data = None
@@ -333,15 +378,18 @@ class DataCollectorAndProcessor:
                         self.priceData = self.priceData.iloc[-self._df_size_limit:]
                     else:
                         logger.warning(f"Data Saver has not stored the recent price data, since the data size is below the threshold: {self.priceData.shape[0]}")
+
+                if (data is not None):
+                    self._memory_saver.write(data)
+                    logger.info(f"Data Saver has stored the recent price data: size: {data.shape[0]} rows and {data.shape[1]} columns")
+                time.sleep(149.9) # make an adjustment.
+
+                return None
+            
             except Exception as e:
                 logger.warning(f"{__name__}: _resize_df - Exception caused: {e}")
 
-            if (data is not None):
-                self._memory_saver.write(data)
-                logger.info(f"Data Saver has stored the recent price data: size: {data.shape[0]} rows and {data.shape[1]} columns")
-            time.sleep(149.9) # make an adjustment.
-
-        return
+        return None
     
     """
     ######################################################################################################################
