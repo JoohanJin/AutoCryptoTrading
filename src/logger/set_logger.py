eb5387b86e21f3e29a0ff5030bb9c7c5a601e273
Joohan Jin <wngks3146@naver.com>
2025-01-02 20:00:18 +0800

    feat: implemented the TimeRotatedFileGenerated Logger

--- src/logger/set_logger.py
+++ src/logger/set_logger.py
@@ -1,16 +1,44 @@
 import logging
+from logging.handlers import TimedRotatingFileHandler
 import time
 from functools import wraps
+import os
 
 #TODO: Need to implement the dynamic logging file generator.
 # config the basic log format
-logging.basicConfig(
-    filename=f"log/{time.strftime('%Y-%m-%d', time.localtime(time.time()))}.log",
-    encoding='utf-8',
-    format="%(asctime)s - %(levelname)s - %(message)s",
-    level=logging.INFO
-    )
-logger = logging.getLogger("future logger")
+# logging.basicConfig(
+#     filename=f"log/{time.strftime('%Y-%m-%d', time.localtime(time.time()))}.log",
+#     encoding='utf-8',
+#     format="%(asctime)s - %(levelname)s - %(message)s",
+#     level=logging.INFO
+# )
+log_dir: str = "log"
+if (not os.path.exists(log_dir)):
+    os.makedirs(log_dir)
+
+log_filename: str = os.path.join(log_dir, f"{time.strftime('%Y-%m-%d', time.localtime(time.time()))}.log")
+
+# create a TimedRotatingFileHandler object based on the current date, which rotates logs at midnight
+handler = TimedRotatingFileHandler(
+    log_filename,
+    when="midnight",
+    interval=1,
+    encoding='utf-8'
+)
+handler.suffix = "%Y-%m-%d"
+
+# define the log format
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+
+# create a logger
+logger = logging.getLogger("TradingBot Logger")
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
+
+# Prevent duplicate handlers if the logger is imported multiple times.
+logger.propagate = False
+
 
 # define log_decorator for the function, future usage consideration
 def log_decorator(func):
