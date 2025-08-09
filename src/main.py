e936c3261b37837f60d28b0fb77163632345149d
Joohan JIN <joohanjin@Joohans-MacBook-Air.local>
2025-08-09 20:10:29 +0800

    chord: code structure changed

--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
 from manager.system_manager import SystemManager
 from logger.set_logger import operation_logger
 
-
 def main():
     try:
         # Since __init__() for every class will activate them, no need to do anything here.
         # TODO: but expliciti activation should be better. In the near future.
         main_system_manager: SystemManager = SystemManager()
         operation_logger.info(
-            f"{main_system_manager} has been started without any problem."
+            f"{main_system_manager} has been started."
         )
     except Exception as e:
         operation_logger.critical(
