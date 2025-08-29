9b4669a15e765a983708fd5a82290245fd596eba
JoohanJIN <wngks8730@gmail.com>
2025-08-29 23:00:00 +0900

    feat: changed the structure of Signal data structure

--- src/object/signal.py
+++ src/object/signal.py
@@ -3,6 +3,7 @@ from typing import List, Dict, Union, Tuple
 from enum import IntFlag
 import time
 
+
 class TradeSignal(IntFlag):
     # state management using the bit manipulation.
     SHORT_TERM_BUY = 1  # 0001
@@ -13,19 +14,45 @@ class TradeSignal(IntFlag):
 
 
 class Signal:
+    '''
+    ##############################################################
+    #                         Static Method                      #
+    ##############################################################
+    '''
     @staticmethod
     def generate_timestamp() -> int:
         return int(time.time() * 1_000)
 
+    '''
+    data_struct = {
+    "indicator": {
+        "timestamp": <int>, int(time.time() * 1000),
+        "signal": object.TradeSignal
+            # 001: for buy
+            # 010: for sell
+            # 100: for hold -> do nothing
+            # Other signals
+        }
+    }
+    '''
+
     def __init__(
         self: 'Signal',
         signal: TradeSignal,
-        timestamp: int = None, # Default input is None
     ) -> None:
         """
         func __init__:
             - Create an indicator instance with the given signal and timestamp.
         """
-        self.timestamp = Signal.generate_timestamp() if (not timestamp) else timestamp
-        self.signal = signal
+        self._timestamp: int = signal.get("timestamp", Signal.generate_timestamp())
+        self._signal: TradeSignal = signal
+
         return None
+
+    @property  # timestamp getter
+    def timestamp(self) -> int:
+        return self._timestamp
+
+    @property  # signal getter.
+    def signal(self) -> TradeSignal:
+        return self._signal
