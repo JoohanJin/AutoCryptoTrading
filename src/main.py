8cd4b7f443fa70ba9d45780ba7105e338aad8580
JoohanJIN <wngks8730@gmail.com>
2025-10-14 22:45:58 +0800

    feat: chnaged the import path

--- src/main.py
+++ src/main.py
@@ -1,17 +1,20 @@
+from pathlib import Path
+from dotenv import load_dotenv
+
 from manager.system_manager import SystemManager
 from logger.set_logger import operation_logger
-from dotenv import load_dotenv
 
 
 def main():
     try:
         # ? Since __init__() for every class will activate them, no need to do anything here.
-        # TODO: but expliciti activation should be better. In the near future.
-        load_dotenv()
-        main_system_manager: SystemManager = SystemManager()
-        operation_logger.info(
-            f"{main_system_manager} has been started."
-        )
+        project_root = Path(__file__).resolve().parents[1]
+        load_dotenv(project_root / ".env")
+
+        # main_system_manager: SystemManager = SystemManager()
+        # operation_logger.info(
+        #     f"{main_system_manager} has been started."
+        # )
     except Exception as e:
         operation_logger.critical(
             f"{__name__}: function main() has raised an Unexpected error starting the system: {e}"
